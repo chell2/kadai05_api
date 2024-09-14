1633c4e6cb69dbbae61c2c226391baaa473a2b76
chell2 <rei1259@gmail.com>
2024-09-15 08:03:10 +0900

    page change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { getAuth, signOut } from 'firebase/auth';
+// import { getAuth, signOut } from 'firebase/auth';
 import { useAuth } from '@/app/hooks/useAuth';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
@@ -8,53 +8,42 @@ import { useEffect } from 'react';
 export default function Home() {
   const { user, loading } = useAuth();
   const router = useRouter();
-  const auth = getAuth();
+  // const auth = getAuth();
 
-  const handleLogout = async () => {
-    try {
-      await signOut(auth);
-      router.push('/signin');
-    } catch (error) {
-      console.error('Logout failed:', error);
-    }
-  };
+  // const handleLogout = async () => {
+  //   try {
+  //     await signOut(auth);
+  //     router.push('/signin');
+  //   } catch (error) {
+  //     console.error('Logout failed:', error);
+  //   }
+  // };
 
   useEffect(() => {
-    if (!loading && !user) {
+    if (!loading && user) {
+      switch (user.role) {
+        case 'admin':
+          router.push('/settings');
+          break;
+        case 'editor':
+          router.push('/seat');
+          break;
+        case 'viewer':
+          router.push('/seat-viewer');
+          break;
+        default:
+          router.push('/signin');
+          break;
+      }
+    } else if (!loading && !user) {
       router.push('/signin');
     }
   }, [loading, user, router]);
 
-  if (loading) return <p>Loading...</p>;
-
   return (
     <div>
-      <h1>
-        Welcome, {user?.role} {user?.email}!
-      </h1>
-
-      <button onClick={handleLogout}>Logout</button>
-
-      {user?.role === 'admin' && (
-        <div>
-          <h2>Adminページ</h2>
-          <a href="/seat">編集画面へ</a>
-        </div>
-      )}
-
-      {user?.role === 'editor' && (
-        <div>
-          <h2>Editorページ</h2>
-          <a href="/seat">編集画面へ</a>
-        </div>
-      )}
-
-      {user?.role === 'viewer' && (
-        <div>
-          <h2>Viewerページ</h2>
-          <p>閲覧のみ可能なページへ</p>
-        </div>
-      )}
+      <p>Loading...</p>
+      {/* <button onClick={handleLogout}>Logout</button> */}
     </div>
   );
 }
